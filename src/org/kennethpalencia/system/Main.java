97b3d0082185c8cea91d6873aab4876ccea1c12e
Kenneth Palencia <kpalencia-2023259@kinal.edu.gt>
2026-06-12 11:43:52 -0600

    003 - Se agregaron el controller y el view

---
+++ src/org/kennethpalencia/system/Main.java
@@ -0,0 +1,30 @@
+package org.kennethpalencia.system;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+
+        
+
+        
+    }
+
+        @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //nodos 
+        VBox root = new VBox(); 
+        //escena 
+        Scene escena = new Scene(root);
+      //mostrar escenario principal
+      escenarioPrincipal.setTitle("asdfasdf");
+      escenarioPrincipal.setScene(escena);
+      escenarioPrincipal.show();
+    }
+}
---
+++ src/org/kennethpalencia/view/Calculadoraview.java
@@ -0,0 +1,5 @@
+package org.kennethpalencia.view;
+
+public class Calculadoraview {
+    
+}
