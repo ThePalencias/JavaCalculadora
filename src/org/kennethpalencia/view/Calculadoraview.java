4798b1ae18b0fe9653fbd7b912c3a2f8d4516daf
Kenneth Palencia <kpalencia-2023259@kinal.edu.gt>
2026-06-12 11:55:45 -0600

    004 - Se hicieron cambios en el view

--- src/org/kennethpalencia/view/Calculadoraview.java
+++ src/org/kennethpalencia/view/Calculadoraview.java
@@ -1,5 +1,20 @@
 package org.kennethpalencia.view;
 
+import java.awt.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 public class Calculadoraview {
+    
+    private VBox view;
+    private Label panralla;
+    private GridPane cuadroBotones;
+    //controller
+    
+    public Calculadoraview(){
+        view = new VBox();
+        Label pantalla = new Label("0");
+        cuadroBotones = new GridPane();
+    }
     
 }
